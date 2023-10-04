c669434efdbbd58be1e06760d58470f051b4b33a
Emerson Romana Senna <133699469+Minfermis@users.noreply.github.com>
2023-10-03 23:13:56 -0300

    Add files via upload

---
+++ estruturaRepeticaoEx1.js
@@ -0,0 +1,8 @@
+// Crie um programa que dado um número imprima a sua tabuada. 
+
+
+const num = 5;
+for (let i = 1; i <= 10 ; i++){
+    const resultado = i * num;
+    console.log(`${i} X ${num} = ${resultado}`);
+}
